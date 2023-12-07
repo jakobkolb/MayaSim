5ea6ec38f2ef1823628393413273696e647188ee
fkuehlein <fritz.kuehlein@student.uni-halle.de>
2023-12-07 10:58:36 +0100

    MAINT: fix fortran compilation with `f2py`
    - adapt to `gfortran`, had probably used `gcc` before
    - related: #1

--- mayasim/model/_ext/f90makefile.sh
+++ mayasim/model/_ext/f90makefile.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
+# 
+# execute this file from command line to compile f90routines.f90
+# and create extension module via numpy.f2py.
+# `--f90flags` contains flags for the gfortran compiler
 
-f2py    --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
-f2py3   --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
-f2py3.5 --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
-
-
-#f2py    --f90flags='-g -fbounds-check' -lgomp -c  f90routines.f90 -m f90routines
-#f2py3   --f90flags='-g -fbounds-check' -lgomp -c  f90routines.f90 -m f90routines
-#f2py3.5 --f90flags='-g -fbounds-check' -lgomp -c  f90routines.f90 -m f90routines
-
+f2py --f90flags='-O3 -march=native' -lgomp -c f90routines.f90 -m f90routines
